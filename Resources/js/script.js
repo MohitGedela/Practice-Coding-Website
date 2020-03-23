494ffcf9e345d3ae157c422991dcddc402475877
Mohit2006 <62566794+Mohit2006@users.noreply.github.com>
2020-03-23 15:00:51 -0400

    First project commit
    First project commit
    which includes udemy course until lesson-65

---
+++ Resources/js/script.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+    
+    /* ----------------------------------------- */
+    /* STICKY NAVIGATION */
+    /* ----------------------------------------- */
+    
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset:'60px;'
+    });
+    
+    /* ----------------------------------------- */
+    /* SCROLL ON BUTTONS */
+    /* ----------------------------------------- */
+    
+    $('.js--scroll-to-plans').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000) 
+    });
+
+    $('.js--scroll-to-start').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000) 
+    });    
+    
+    /* ----------------------------------------- */
+    /* NAVIGATION SCROLL */
+    /* ----------------------------------------- */
+    
+    $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus(); 
+          };
+        });
+      }
+    }
+  });
+    
+    /* ----------------------------------------- */
+    /* ANIMATIONS ON SCROLL */
+    /* ----------------------------------------- */  
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-6').waypoint(function(direction) {
+        $('.js--wp-6').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+
+
+});
